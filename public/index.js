7788702f5bbeacb7e49f516d86aa5018379bca2f
eprim7 <evan.primasing7@gmail.com>
2024-05-15 12:31:56 -0500

    initial commit

---
+++ public/index.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAf6y2dqR0MSZ22B6xZHDJJqThxkzEjo-c",
+    authDomain: "rex-chatbot-5208f.firebaseapp.com",
+    projectId: "rex-chatbot-5208f",
+    storageBucket: "rex-chatbot-5208f.appspot.com",
+    messagingSenderId: "704518462997",
+    appId: "1:704518462997:web:e68055cfcd88a5ce45b81c"
+  };
+
+
+firebase.initializeApp(firebaseConfig)
